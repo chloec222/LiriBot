9178232ada611eb93d04ba2fba8e6f3e91338d94
Chloe <55222885+chloec222@users.noreply.github.com>
2020-01-19 02:20:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ var Spotify = require("node-spotify-api");
 var fs = require("fs");
 var moment = require("moment");
 var keys = require("./keys.js");
-var spotifyKeys = require("./keys.js")
+var spotifyKeys = require("./keys.js");
 var spotify = new Spotify(spotifyKeys.spotify);
 
-// let spotifyKeys = require("./keys.js")
-// let spotify = new Spotify(spotifyKeys.spotify);
 
 // variables for the arguments to be entered by the user in Liri
 var appCommand = process.argv[2];
@@ -45,19 +43,20 @@ function liriRun(appCommand, userSearch){
 
 // Spotify API search function
 function getSpotify(songName){
-    var spotify = new Spotify(spotifyKeys.spotify);
+    
     if (!songName){
-        songName = "The Sign";
+        songName = "Over the Rainbow";
     }
     spotify.search({type: 'track', query: songName}, function(err, data){
         if (err) {
             return console.log('Error: ' + err);
         }
-        console.log("===================");
-        console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name + "\r\n");
-        console.log("Song Name: " + data.tracks.items[0].name + "\r\n");
+        console.log("========== Searching for... ==========");
+        console.log("Artist: " + data.tracks.items[0].album.artists[0].name + "\r\n");
+        console.log("Song: " + data.tracks.items[0].name + "\r\n");
         console.log("Song Preview Link: " + data.tracks.items[0].href + "\r\n");
         console.log("Album: "+ data.tracks.items[0].album.name + "\r\n");
+        console.log("========== Search completed ==========");
 
 // append text into log.txt
     var logSong = "=====Begin Spotify Log Entry=====" + "\nArtist: " + data.tracks.items[0].album.artists[0].name;
@@ -68,22 +67,24 @@ function getSpotify(songName){
     });
 }
 
-// function to search Bands in Town API
+
+// Bands API search function
 function getBandsInTown(artist){
     
     var bandQueryURL = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp";
 
     axios.get(bandQueryURL).then(
         function (response){
-            console.log("=====================");
-            console.log("Name of the venue: " + response.data[0].venue.name + "\r\n");
+            console.log("========== Searching for... ==========");
+            console.log("Venue: " + response.data[0].venue.name + "\r\n");
             console.log("Venue Location: " + response.data[0].venue.city + "\r\n");
-            console.log("Date of event: " + moment(response.data[0].venue.datetime).format("MM-DD-YYYY") + "\r\n");
+            console.log("Event Date: " + moment(response.data[0].venue.datetime).format("MM-DD-YYYY") + "\r\n");
+            console.log("========== Search completed ==========");
         })
         .catch(function (error) {
             console.log(error);
         
-            var logConcert = "======Begin Concert Log Entry======" +"\nName of the musician: " + userSearch + "\nName of the venue: " + response.data[0].venue.name;
+            var logConcert = "========== Begin Concert Log Entry ==========" +"\nArtist: " + userSearch + "\nVenue: " + response.data[0].venue.name;
 
             fs.appendFile("log.txt", logConcert, function(err){
                 if(err) throw err;
@@ -95,22 +96,23 @@ function getBandsInTown(artist){
 // function to search OMDB API
 function getOMDB(movie){
     if(!movie){
-        movie = "Mr. Nobody";
+        movie = "Top Gun";
     }
     var movieQueryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
     axios.request(movieQueryURL).then(
         function(response){
-            console.log("====================");
+            console.log("========== Searching for... ==========");
             console.log("Title: " + response.data.Title + "\r\n");
             console.log("Year released: " + response.data.Year + "\r\n");
             console.log("IMDB Rating: " + response.data.imdbRating + "\r\n");
             console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\r\n");
-            console.log("Conutry produced: " + response.data.Country + "\r\n");
+            console.log("Country produced: " + response.data.Country + "\r\n");
             console.log("Language: " + response.data.Language + "\r\n");
             console.log("Plot: " + response.data.Plot + "\r\n");
             console.log("Actors: " + response.data.Actors + "\r\n");
+            console.log("========== Search completed ==========");
 
-            var logMovie = "============Begin Movie Log Entry============" +"/nMovie title: " + response.data.Title + "/nYear released: " + response.data.Year;
+            var logMovie = "========== Begin Movie Log Entry ==========" +"/nMovie title: " + response.data.Title + "/nYear released: " + response.data.Year;
 
             fs.appendFile("log.txt", logMovie, function(err){
                 if(err) throw err;
@@ -124,9 +126,11 @@ function getRandom(){
         if(error){
             return console.log(error);
         } else {
+            console.log("========== Searching for... ==========");
             console.log(data);
             var randomData = data.split(" .");
             liriRun(randomData[0], randomData[1]);
+            console.log("========== Search completed ==========");
         }
     });
 }
